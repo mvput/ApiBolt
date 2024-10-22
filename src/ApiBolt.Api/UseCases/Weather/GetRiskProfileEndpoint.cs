9828ea2c5bab5cb8c15f55d0ba22cbc6a711ede2
Martijn van Put <martijn@vanput.net>
2024-10-22 22:51:54 +0200

    Update sample

---
+++ src/ApiBolt.Api/UseCases/Weather/GetRiskProfileEndpoint.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) GRCcontrol B.V. All rights reserved.
+
+using ApiBolt.Abstractions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiBolt.Api.UseCases.Weather;
+
+internal class GetWeatherEndpoint : IApiEndpoint
+{
+    [ApiEndpoint(ApiEndpointType.Get, "/weather")]
+    public async Task<string> GetWeatherAsync()
+    {
+        return $"Current weather is 14 degrees celcius";
+    }
+}
